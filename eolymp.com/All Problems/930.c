00f98ba07d204d23ee7d0f8ee525b533f4bd21f0
Prabesh Aryal <prabesharyal24@gmail.com>
2023-04-27 21:28:48 +0400

    HackerRank

---
+++ eolymp.com/All Problems/930.c
@@ -0,0 +1,12 @@
+// #include <stdio.h>
+
+// int main(){
+//     char phoneNumber[20];
+//     scanf("%c",&phoneNumber);
+//     int remaining[9];
+//     for (int i = 0; i<20; i++){
+//         printf("%c",phoneNumber[i]);
+//     }
+//     return 0;
+// }
+
